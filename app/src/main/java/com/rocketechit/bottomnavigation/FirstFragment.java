df807d17d84b4e5ef3c08dce998b33b182717cce
Imran sk <imran.bu96@gmail.com>
2019-01-14 12:59:54 +0600

    Add Time Count Down in FirstFragment

--- app/src/main/java/com/rocketechit/bottomnavigation/FirstFragment.java
+++ app/src/main/java/com/rocketechit/bottomnavigation/FirstFragment.java
@@ -1,15 +1,21 @@
 package com.rocketechit.bottomnavigation;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.concurrent.TimeUnit;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class FirstFragment extends Fragment {
+    TextView textView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -19,5 +25,39 @@ public class FirstFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        textView=view.findViewById(R.id.first_fragment_TV);
+
+        //Time Count Down
+        CountDownTimer  countDownTimer = new CountDownTimer((long) (1*60*1000), 1000) {
+            public void onTick(long millisUntilFinished) {
+                long millis = millisUntilFinished;
+                //Convert milliseconds into hour,minute and seconds
+                String hms = String.format("%02d:%02d:%02d", TimeUnit.MILLISECONDS.toHours(millis), TimeUnit.MILLISECONDS.toMinutes(millis) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millis)), TimeUnit.MILLISECONDS.toSeconds(millis) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis)));
+                textView.setText(hms);//set text
+            }
+            public void onFinish() {
+                textView.setText("TIME'S UP!!"); //On finish change timer text
+            }
+        }.start();
+
+
+       /* new CountDownTimer(30000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+//                textView.setText("seconds remaining: " + millisUntilFinished / 1000);
+
+                //Convert milliseconds into hour,minute and seconds
+                String hms = String.format("%02d:%02d:%02d", TimeUnit.MILLISECONDS.toHours(millisUntilFinished),
+                        TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millisUntilFinished)), TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished)));
+
+                textView.setText(hms);//set text
+            }
+
+            public void onFinish() {
+                textView.setText("done!");
+            }
+        }.start();*/
+
     }
 }
